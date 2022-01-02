97d3a4394a497445722ce95ce8ab13e6fea37943
Simon Tallmyr <simon@tallmyr.se>
2022-01-02 16:04:40 +0000

    Day 3 tests

---
+++ tests/test_day3.py
@@ -0,0 +1,9 @@
+from assignments import day3
+
+
+def test_solution1():
+    assert day3.solution1("test_inputs", 5) == (22, 9, 198)
+
+
+def test_solution2():
+    assert day3.solution2("test_inputs", 5) == (23, 10, 230)
