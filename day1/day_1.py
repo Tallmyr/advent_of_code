c0fea49d565501cc3b5753195be20a468db14b22
Simon Tallmyr <simon@tallmyr.se>
2022-01-02 01:57:11 +0000

    Init Day 1

---
+++ day1/day_1.py
@@ -0,0 +1,32 @@
+def solution1():
+    with open("day1/list.txt", "r") as file:
+        list1 = file.read().splitlines()
+
+        increases = 0
+
+        for i, x in enumerate(list1[1:]):
+            if int(x) > int(list1[i]):
+                increases += 1
+        
+        return(increases)
+
+def solution2():
+    with open("day1/list.txt", "r") as file:
+        list1 = file.read().splitlines()
+
+        increases = 0
+
+        for i, x in enumerate(list1):
+            try:
+                blocka = int(x) + int(list1[i+1]) + int(list1[i+2])
+                blockb = int(list1[i+1]) + int(list1[i+2]) + int(list1[i+3])
+                
+                if blocka < blockb:
+                    increases += 1
+            except IndexError:
+                print("Out of lines!")
+        
+        return(increases)
+
+
+print(solution2())
