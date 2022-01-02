8145748c645c7d0be05fea36d4cf028c4c8e4e09
Simon Tallmyr <simon@tallmyr.se>
2022-01-02 02:20:39 +0000

    Day 2 complete

--- day2/day_2.py
+++ day2/day_2.py
@@ -18,4 +18,25 @@ def solution1():
         return f"Distance: {dist}, Depth: {depth}. Result: {dist * depth}"
 
 
-print(solution1())
+def solution2():
+    with open("day2/input.txt", "r") as file:
+        list1 = file.read().splitlines()
+
+        dist, depth, aim = 0, 0, 0
+
+        for x in list1:
+            instruct, value = x.split(" ")
+            if instruct == "forward":
+                dist += int(value)
+                depth += (aim * int(value))
+            elif instruct == "down":
+                aim += int(value)
+            elif instruct == "up":
+                aim -= int(value)
+            else:
+                raise ValueError(f"Incorrect instruction. Instruction found was: {instruct}")
+
+        return f"Distance: {dist}, Depth: {depth}. Result: {dist * depth}"
+
+
+print(solution2())
